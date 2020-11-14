6247d3f5ae04e74538902c7d09ce5cadcbbe328e
ryandejaegher <dejaegher.ryan@gmail.com>
2020-11-14 11:58:55 -0600

    update

--- assets/theme.js
+++ assets/theme.js
@@ -9682,9 +9682,25 @@ window.addEventListener('load', function(){
       prev: '.glider-prev',
       next: '.glider-next', 
     },
-    slidesToShow:3,
-    slidesToScroll:3,
+    slidesToShow:1,
+    slidesToScroll:1,
     rewind: true,
-    scrollLock: true
+    scrollLock: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow:2,
+          slidesToScroll:2
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow:3,
+          slidesToScroll:3
+        }
+      }
+    ]
   })
 })
